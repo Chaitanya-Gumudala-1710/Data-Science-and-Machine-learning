75868859d982a5da5992c96d9129834f923ac196
Chaitanya Gumudala <chaitanyagumudala961@gmail.com>
2024-02-19 14:56:29 +0100

    Documented matplotlib liveplots

--- .history/Matplotlib/Graphs/LiveData/data_generation_20240219132620.py
+++ .history/Matplotlib/Graphs/LiveData/data_generation_20240219132620.py
@@ -1,41 +1,27 @@
-# Importing the necessary modules
 import csv
 import random
 import time
 
-# Initializing variables
 x_value = 0
 total_1 = 1000
 total_2 = 1000
 fieldnames = ["x_value", "total_1", "total_2"]
 
-# Creating and writing the header to the CSV file
 with open('data.csv', 'w') as csv_file:
     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     csv_writer.writeheader()
 
-# Infinite loop to continuously generate and write data to the CSV file
 while True:
     with open('data.csv', 'a') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        
-        # Creating a dictionary with the data to be written
         info = {
             "x_value": x_value,
             "total_1": total_1,
             "total_2": total_2
         }
-        
-        # Writing the data to the CSV file
         csv_writer.writerow(info)
-        
-        # Printing the values for debugging purposes
         print(x_value, total_1, total_2)
-        
-        # Updating the variables for the next iteration
         x_value += 1
         total_1 = total_1 + random.randint(-6, 8)
         total_2 = total_2 + random.randint(-5, 6)
-    
-    # Pausing the execution for 1 second before the next iteration
     time.sleep(1)
--- .history/Matplotlib/Graphs/LiveData/live_data_plotting_20240219132620.py
+++ .history/Matplotlib/Graphs/LiveData/live_data_plotting_20240219132620.py
@@ -9,38 +9,15 @@ y_vals = []
 index = count()
 
 
-import pandas as pd
-import matplotlib.pyplot as plt
-
 def animate(i):
-    """
-    Function to animate the live data plotting.
-
-    Parameters:
-    - i (int): The frame number for the animation.
-
-    Returns:
-    - None
-    """
-    # Read the data from the CSV file
     data = pd.read_csv('data.csv')
-
-    # Extract the x and y values from the data
     x = data['x_value']
     y1 = data['total_1']
     y2 = data['total_2']
-
-    # Clear the current plot
     plt.cla()
-
-    # Plot the data for Channel 1 and Channel 2
     plt.plot(x, y1, label='Channel 1')
     plt.plot(x, y2, label='Channel 2')
-
-    # Add legend to the plot
     plt.legend(loc='upper left')
-
-    # Adjust the layout of the plot
     plt.tight_layout()
 
 
